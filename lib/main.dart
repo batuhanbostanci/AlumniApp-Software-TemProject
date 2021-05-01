a4417fed706fba1144bad24f97703cd2b001bcf9
muham <muhammedemre.cunkus@agu.edu.tr>
2021-05-01 18:09:54 +0300

    change to red

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.teal,
+      color: Colors.red,
     );
   }
 }
