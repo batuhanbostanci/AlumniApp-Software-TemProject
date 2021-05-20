f4b741e3affecba0499aef3837cafb8557180aae
Tacettin Batuhan Bostanci <batuhanbostanci2@hotmail.com>
2021-05-20 19:19:18 +0300

    changing primary color

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'ui/homepage/home_page.dart';
 
-void main() => runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -12,7 +17,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         scaffoldBackgroundColor: Color(0xFFFFFFFF),
-        primaryColor: Color(0xFFFF4700),
+        primaryColor: Colors.teal,
       ),
       home: HomePage(),
     );
